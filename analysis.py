c14d206c3b732f56c42273a67cc1573c8114913d
printf-genna <154890661+printf-genna@users.noreply.github.com>
2025-06-10 00:58:45 -0300

    spotify  script+reduced dataset

---
+++ analysis.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: myenv
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import kagglehub
+import os
+import scipy.stats as stats
